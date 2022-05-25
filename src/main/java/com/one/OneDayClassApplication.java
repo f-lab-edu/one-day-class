84ec804cc595c9a1c907ab8cee130a3d338e313c
SANGHYUN KIM <haan823@naver.com>
2022-05-26 01:32:35 +0900

    feat: spring boot 프로젝트 생성

---
+++ src/main/java/com/one/OneDayClassApplication.java
@@ -0,0 +1,13 @@
+package com.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDayClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneDayClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/one/OneDayClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneDayClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
