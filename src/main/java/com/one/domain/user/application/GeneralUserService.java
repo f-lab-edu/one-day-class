af4aa5642b3d95a60cd58e0306c7a9b1ecd537a5
SANGHYUN KIM <haan823@naver.com>
2022-06-03 18:21:11 +0900

    refactor: UserService 인터페이스로 변경

---
+++ src/main/java/com/one/domain/user/application/GeneralUserService.java
@@ -0,0 +1,19 @@
+package com.one.domain.user.application;
+
+import com.one.domain.user.exception.UserNotFoundException;
+import com.one.domain.user.mapper.UserMapper;
+import com.one.domain.user.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor // final이나 @NotNull이 붙은 필드의 생성자를 생성해주는 롬복 어노테이션
+public class GeneralUserService implements UserService {
+
+    private final UserMapper userMapper;
+
+    public User getUserById(final int id) {
+        return userMapper.selectUserById(id).orElseThrow(() -> new UserNotFoundException());
+    }
+
+}
