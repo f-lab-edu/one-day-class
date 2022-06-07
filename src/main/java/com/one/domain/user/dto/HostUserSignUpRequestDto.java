26c88a95804259b46d2cce2a7d3129a2f574cd47
SANGHYUN KIM <haan823@naver.com>
2022-06-08 01:03:08 +0900

    fix: 회원가입 기능에서 사용하는 dto 필드값 수정

--- src/main/java/com/one/domain/user/dto/HostUserSignUpRequestDto.java
+++ src/main/java/com/one/domain/user/dto/HostUserSignUpRequestDto.java
@@ -2,6 +2,6 @@ package com.one.domain.user.dto;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public record HostUserSignUpRequestDto(String userId, String password, String password2, String name, String phoneNumber, MultipartFile multipartFile) {
+public record HostUserSignUpRequestDto(String userId, String password, String password2, String name, String phoneNumber, int bigCategoryId, MultipartFile multipartFile) {
 
 }
--- src/main/java/com/one/domain/user/dto/UserSaveRequestDto.java
+++ src/main/java/com/one/domain/user/dto/UserSaveRequestDto.java
@@ -2,14 +2,27 @@ package com.one.domain.user.dto;
 
 import com.one.domain.user.code.UserStatus;
 import com.one.domain.user.code.UserType;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-public record UserSaveRequestDto(String userId, Integer imageFileId, String password, String name, String phoneNumber, UserType userType, UserStatus userStatus) {
+@Getter
+@RequiredArgsConstructor
+public final class UserSaveRequestDto {
+    private final Integer id;
+    private final String userId;
+    private final Integer imageFileId;
+    private final String password;
+    private final String name;
+    private final String phoneNumber;
+    private final UserType userType;
+    private final UserStatus userStatus;
 
     public static UserSaveRequestDto of(HostUserSignUpRequestDto hostUserSignUpRequestDto, int imageFileId) {
-        return new UserSaveRequestDto(hostUserSignUpRequestDto.userId(), imageFileId, hostUserSignUpRequestDto.password(), hostUserSignUpRequestDto.name(), hostUserSignUpRequestDto.phoneNumber(), UserType.H, UserStatus.B);
+        return new UserSaveRequestDto(null, hostUserSignUpRequestDto.userId(), imageFileId, hostUserSignUpRequestDto.password(), hostUserSignUpRequestDto.name(), hostUserSignUpRequestDto.phoneNumber(), UserType.H, UserStatus.B);
     }
 
     public static UserSaveRequestDto of(GuestUserSignUpRequestDto guestUserSignUpRequestDto) {
-        return new UserSaveRequestDto(guestUserSignUpRequestDto.userId(), null, guestUserSignUpRequestDto.password(), guestUserSignUpRequestDto.name(), guestUserSignUpRequestDto.phoneNumber(), UserType.G, UserStatus.A);
+        return new UserSaveRequestDto(null, guestUserSignUpRequestDto.userId(), null, guestUserSignUpRequestDto.password(), guestUserSignUpRequestDto.name(), guestUserSignUpRequestDto.phoneNumber(), UserType.G, UserStatus.A);
     }
+
 }
