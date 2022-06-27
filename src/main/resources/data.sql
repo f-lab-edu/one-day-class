2244a42c93f021abece6180f5289a55b596ebf70
SANGHYUN KIM <haan823@naver.com>
2022-06-27 18:09:35 +0900

    chore: 매퍼 id 매핑 오류 수정, 유저 테스트 데이터 추가 쿼리 작성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
-insert into user values(null, 1, 'test', '1234', '테스트', '01012345678', 0, 0, now(), now());
+insert into user values(null, 1, 'test', 'test', '테스트', '01012345678', 0, 0, now(), now());
+insert into user values(null, 2, 'test2', 'test', '테스트', '01012345678', 1, 1, now(), now());
+insert into user values(null, 3, 'test3', 'test', '테스트', '01012345678', 2, 0, now(), now());
